fa9051d0eca169569df0d6aaf4402109b625efd9
Noel Guillen <noelblas274@gmail.com>
2024-05-08 10:57:41 -0400

    database tables and structure done

--- car-dealership.sql
+++ car-dealership.sql
@@ -7,7 +7,7 @@ CREATE TABLE `dealerships` (
     `dealership_id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(100),
     `address` VARCHAR(100),
-    `phone` VARCHAR(12)
+    `phone` VARCHAR(20)
 );
 
 CREATE TABLE `vehicle` (
@@ -19,7 +19,7 @@ CREATE TABLE `vehicle` (
     `color` VARCHAR(50),
     `odometer` INT,
     `price` DOUBLE,
-    `sold` VARCHAR(5)
+    `sold` VARCHAR(3)
 );
 
 CREATE TABLE `inventory` (
@@ -41,3 +41,37 @@ CREATE TABLE `lease_contracts` (
     `vin` INT,
     FOREIGN KEY (`vin`) REFERENCES `vehicle` (`vin`)
 );
+
+INSERT INTO `dealerships`(`name`, `address`, `phone`)
+VALUES
+	('Hippo Motors', '123 Hippo Ave, Hippo City, HP1234', '555-HIPPO-1234'),
+    ('Piggy Motors','456 Piggy Lane, Piglet Town, PG5678','555-OINK-5678'),
+    ('Gator Motors','789 Swamp Road, Gatorville, GA9012','555-SNAP-9012');
+
+INSERT INTO `vehicle`(`vin`, `year`, `make`, `model`, `type`, `color`, `odometer`, `price`, `sold`)
+VALUES
+    (123456, 2022, 'Toyota', 'Camry', 'Sedan', 'Blue', 5000, 25000, 'No'),
+    (234567, 2021, 'Honda', 'Civic', 'Sedan', 'Red', 8000, 22000, 'No'),
+    (345678, 2020, 'Ford', 'F-150', 'Truck', 'Black', 10000, 35000, 'Yes'),
+	(456789, 2023, 'Tesla', 'Model Y', 'SUV', 'White', 2000, 60000, 'No'),
+    (567890, 2022, 'Lamborghini', 'Aventador', 'Sports Car', 'Yellow', 100, 300000, 'No'),
+    (678901, 2021, 'Porsche', '911', 'Coupe', 'Black', 5000, 100000, 'Yes');
+
+INSERT INTO `inventory`(`dealership_id`, `vin`)
+VALUES
+(1, 123456),
+(1, 234567),
+(2, 345678),
+(2, 456789),
+(3, 567890),
+(3, 678901);
+
+INSERT INTO `sales_contracts`(`vin`)
+VALUES
+	(345678),
+    (678901);
+
+
+
+
+
